88eba94b40bcdebeba15c7750f6e2fb137ee558c
Jonatan Rodriguez <jonatanfr86@gmail.com>
2022-05-30 22:19:44 -0300

    Agregamos una tabla al proyecto

--- js/cotizar.js
+++ js/cotizar.js
@@ -1,221 +1,308 @@
-/*Cotizador*/
+
+/*
+ENTIDADES
+*/
+
 
 class Cotizador {
 
-    constructor(marca,modelo,year,plan){
-        this.marca =marca;
+    constructor(marca, modelo, year, plan) {
+        this.marca = marca;
         this.modelo = modelo;
         this.year = year;
         this.plan = plan;
     }
+
+
 }
 
-
+/*
+VARIABLES
+*/
 
 let listaDeCotizaciones = [];
-let btnCotizar = document.getElementById("btnCotizar");
+const btnCotizar = document.getElementById("btnCotizar");
+const btnPrincipal = document.querySelector(".botonPrincipal");
 const form = document.getElementById("form");
-let divResumen=document.querySelector("#resumen");
-let divResultado=document.querySelector("#resultado");
+let divResumen = document.querySelector("#resumen");
+let divResultado = document.querySelector("#resultado");
 let imagenLoading = document.querySelector(".imgLoading");
 const infoPlanes = document.querySelector(".infoPlanes");
-
-
+const btnMisCotizaciones = document.getElementById("btnMisCotizaciones");
+const listacotizacion = document.getElementById("tabla");
+const btnConfirmar = document.getElementById("btnConfirmar");
 
 
 
 /*
-'<h2>Resumen de Cotizacion</h2><ul><li>Marca: ${marca}</li><li>Modelo: ${modelo}</li> <li>Plan: ${plan}</li> <li>Año: ${year}</li></ul>';
-},3000);
+FUNCIONES
 */
-form.addEventListener('submit',function (e){
+
+
+const crearCotizacion = () => {
+
     const marca = document.getElementById('marca').value;
     const modelo = document.getElementById('modelo').value;
     const year = document.getElementById('year').value;
     const plan = document.getElementById('plan').value;
-    
-    const nuevaCotizacion= new Cotizador(marca,modelo,year,plan);
-    
-    e.preventDefault();
-    /* de aca*/
-    
-    
-const crearCotizacion = () => {
-    
-   
-    
 
-    let listaNueva =[];
+    if(marca != "" && modelo !="" && year !="" && plan !=""){
+    const nuevaCotizacion = new Cotizador(marca, modelo, year, plan);
+    
+    let listaNueva = [];
 
-    if(localStorage.getItem("Cotizaciones") != null) {
+    if (localStorage.getItem("Cotizaciones") != null) {
         listaNueva = JSON.parse(localStorage.getItem("Cotizaciones"));
         listaNueva.push(nuevaCotizacion);
-        localStorage.setItem("Cotizaciones",JSON.stringify(listaNueva));
-    }else{
+        localStorage.setItem("Cotizaciones", JSON.stringify(listaNueva));
+    } else {
         listaDeCotizaciones.push(nuevaCotizacion);
-        localStorage.setItem("Cotizaciones",JSON.stringify(listaDeCotizaciones));
+        localStorage.setItem("Cotizaciones", JSON.stringify(listaDeCotizaciones));
         console.log(listaDeCotizaciones);
     }
-    listaDeCotizaciones.push(nuevaCotizacion);
+
     return nuevaCotizacion;
 }
+}
+
+
+const obtenerPlan = (plan) => {
+    return (plan === 'basico') ? 1.20 : 1.50;
+}
+
+
+const calcularMarca = (marca) => {
+
+    let incremento;
+
+
+    if (marca = "Chevrolet") {
+        incremento = 1.20;
+    } else if (marca = "Citroen") {
+        incremento = 1.50;
+    } else if (marca = "Ford") {
+        incremento = 1.30;
+    } else if (marca = "Peugeot") {
+        incremento = 1.60;
+    } else if (marca = "Volkswagen") {
+        incremento = 1.20;
+    };
+
+
+
+    return incremento;
+
+}
+
+const calcularModelo = (modelo) => {
+
+    let incremento;
+
+
+
+    if (modelo = "Cruze") {
+        incremento = 1.50;
+    } else if (modelo = "Ds3") {
+        incremento = 1.30;
+    } else if (modelo = "Focus") {
+        incremento = 1.50;
+    } else if (modelo = "208") {
+        incremento = 1.20;
+    } else if (modelo = "Vento") {
+        incremento = 1.60;
+    };
+
+
+
+    return incremento;
+
+}
+
+
+const diferencia = (year) => {
+
+    const actual = new Date().getFullYear() - year;
+
+    return actual;
+}
+
+
+
+function enviarFormulario() {
+    form.reset();
+    ;
+}
+
+const cotizar = (marca, modelo, year, plan) => {
+
+    let resultado = 7000;
+
+    resultado = calcularMarca(marca) * resultado;
+    resultado = calcularModelo(modelo) * resultado;
+    resultado = obtenerPlan(plan) * resultado;
+    resultado = resultado - (((diferencia(year) * 3) * resultado) / 100);
+
+    return resultado;
+
+}
+
+
+
 
 const verficarStorage = () => {
-    let lista =[];
-    if(localStorage.getItem("Cotizaciones") != null){
+    let lista = [];
+    if (localStorage.getItem("Cotizaciones") != null) {
 
         lista = JSON.parse(localStorage.getItem("Cotizaciones"));
         return lista;
-        }
     }
+}
+
+
 
 const guardar = () => {
+
     crearCotizacion();
-    if (verficarStorage() != undefined){
-        localStorage.setItem("Cotizaciones",JSON.stringify(verficarStorage()));
-    }else{
+    if (verficarStorage() != undefined) {
+        localStorage.setItem("Cotizaciones", JSON.stringify(verficarStorage()));
+    } else {
+
         localStorage.setItem("Cotizaciones", JSON.stringify(listaDeCotizaciones));
     }
+
+    
+
 }
 
-function enviarFormulario(){
-    form.reset();
+const botonCotizar = (marca,modelo,year,plan) => {
+
+    setTimeout(() => {
+
+        if(marca != "" && modelo !="" && year !="" && plan !=""){
+
+            setTimeout(() => {
+                swal({
+                    title: "Excelente!",
+                    text: "La cotización ha sido generada con exito!",
+                    icon: "success",
+                  });
+            },3000);
+
+        }else{
+            setTimeout(() => {
+            swal({
+                title: "Error",
+                text: "Debe completar todos los items para poder cotizar!",
+                icon: "warning",
+                
+              })
+
+        },3000);
+        }
+        
+        
+    },500)
+
+}
+
+const eliminarCotizacion = (number) => {
+    let listaVieja = JSON.parse(localStorage.getItem("Cotizaciones"));
+    let listaFiltrada = listaVieja.filter((obj, index) => index !== number);
+    localStorage.setItem("Cotizaciones", JSON.stringify(listaFiltrada));
+    location.reload();
+}
+
+const cotizacionTotal = () => {
+    let total=0;
+    let precio =0;
+   let itemCotiTotal = document.querySelector('.itemCotizacionTotal');
+
+    verficarStorage().forEach((obj) =>{
+       
+        precio =cotizar(obj.marca, obj.modelo, obj.year, obj.plan);
+      
+        total = total + precio;
+        
+    })
+    
+    itemCotiTotal.innerHTML = 'Total: $'+total;
     
 }
 
-const mostrarResumen = () => {
+
+const imprimirDatos = () => {
+
+    if (verficarStorage() != undefined) {
+        let totalCotizacion;
+        verficarStorage().forEach((obj, index) => {
+            totalCotizacion = 0;
+            totalCotizacion = cotizar(obj.marca, obj.modelo, obj.year, obj.plan);
+            document.getElementById("tabla").innerHTML += `
+            <tr>
+                <td>${obj.marca}</td>
+                <td>${obj.modelo}</td>
+                <td>${obj.year}</td>
+                <td>${obj.plan}</td>
+                <td>${totalCotizacion}</td>
+                <td><button class="borrarCotizacion" onclick=eliminarCotizacion(${index})>X</button></td>
+            </tr>
+        `
+            totalCotizacion = 0;
+            
+        })
+        cotizacionTotal();
+    }
     
-   // document.querySelector("#msj").style.display="none";
-   $("#msj").css("display","none");
+}
+const botonConfirmar = () => {
+    setTimeout(() => {
 
-divResumen.style.backgroundColor="#1f272b";
-divResumen.style.display="block";
-//imagenLoading.style.display="block";
+        swal({
+            title: "Está seguro de confirmar?",
+           
+            buttons: true,
+            dangerMode: true,
+          })
+          .then((willDelete) => {
+            if (willDelete) {
+              swal("Bienvenido a Confidence! En la brevedad un operador se contactará con usted!", {
+                icon: "success",
+              });
+              setTimeout(()=> {
+               
+                location.reload();
+              },3000)
+            } else {
+              swal("Sus cotizaciones seran eliminadas!");
+              setTimeout(()=> {
+                localStorage.clear();
+                location.reload();
+              },2000)
+              
+            }
+          });
+        
+    },500)
+}
 
-$("imagenLoading").css("display","block");
-divResumen.style.margin="60px";
-divResumen.style.marginLeft="0px";
 
-setTimeout(()=>{
+
+/*
+EVENTOS
+*/
+
+
+form.addEventListener('submit', function () {
+    const marca = document.getElementById('marca').value;
+    const modelo = document.getElementById('modelo').value;
+    const year = document.getElementById('year').value;
+    const plan = document.getElementById('plan').value;
+
    
-    imagenLoading.style.display="none";
-    divResumen.style.backgroundColor="rgba(235,228,228,0.034)";
-    
-    divResumen.style.borderRadius="8px";
-    divResumen.style.position="absolute";
-    divResumen.style.width="min(60rem,77%)";
-    divResumen.style.height="230px";
-    divResumen.style.display="block";
-    divResumen.style.marginRight="440px";
-
-    let cotizacionFinal = cotizar(nuevaCotizacion);
-
-    divResumen.innerHTML = '<h3> Resumen de cotización </H3> <ul> <li>Marca: '+ marca +'</li><li>Modelo: '+modelo+'</li> <li>Año: '+year+'</li> <li>Plan: '+plan+'</li><li>Total: $'+cotizacionFinal+'</li></ul>';
-    $("h3").addClass("italic");
-    
-    swal({
-        title: "Felicitaciones!",
-        text: "Has contratado Confidence",
-        icon: "success",
-        button: "Confirmar",
-      });
-
-      const obtenerPlanes = ()=>{
-
-        fetch("infoPlanes")
-            .then(response => response.json())
-            .then((result) => {
-                    let datos = result;
-                    datos.array.forEach(plan => {
-                        infoPlanes.innerHTML +=`
-                            <h3>"Plan: "${plan.plan}</h3>
-                            <p>"Granizo: "${plan.granizo}</p>
-                            <p>"Robo de Ruedas: "${plan.roboDeRuedas}</p>
-                            <p>"Granizo: "${plan.choque}</p>`
-                    });
-            })
-            .catch(error => console.log(error))
-      }
-    
-
-},3000);
-
-
-
-}
-
-const cotizar=(nuevaCotizacion) =>{
-    let marca = nuevaCotizacion.marca;
-    let modelo = nuevaCotizacion.modelo;
-    let year = nuevaCotizacion.year;
-    let plan =nuevaCotizacion.plan;
-    let resultado = 7000;
-
-    const diferenciaYear = diferencia(year);
-    
-    resultado = calcularMarca()*resultado;
-    resultado = calcularModelo()*resultado;
-    resultado-=((diferenciaYear*3)*resultado)/100;
-
-    return resultado;
-    
-}
-
-const obtenerPlan=(plan)=>{
-    return (plan==='basico')?1.20:1.50;
-}
-
-const calcularModelo=(modelo)=>{
-    let incremento;
-
-    switch(modelo){
-        case 'Cruze': incremento=1.50;
-        break;
-        case 'Ds3': incremento=1.30;
-        break;
-        case'Focus': incremento=1.50;
-        break;
-        case'208': incremento=1.20;
-        break;
-        case'Vento':incremento=1.60;
-    }
-     return incremento;
-
-}
-const calcularMarca=(marca)=>{
-let incremento;
-
-    switch(marca){
-        case 'Chevrolet': incremento=1.20;
-        break;
-        case'Citroen': incremento=1.50;
-        break;
-        case'Ford': incremento=1.30;
-        break;
-        case'Peugeot': incremento=1.60;
-        break;
-        case'Volkswagen':incremento=1.20;
-    }
-     return incremento;
-
-    
-}
-
-const diferencia=(year)=>{
-    return new Date().getFullYear()-year;
-}
-
-
-    /*hasta aca*/
     guardar();
-    enviarFormulario();
-    mostrarResumen();
-
-    
+    cotizar(marca, modelo, year, plan);
+    enviarFormulario(); 
+    imprimirDatos();
 })
-
-
-
-
-
+imprimirDatos();
 
